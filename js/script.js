e3948b01122a6253671c6d902685464fe047baf2
IamYuriko <nrtyrk@gmail.com>
2024-04-26 01:48:37 +0900

    second commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function header(rootDir) {
+    $.ajax({
+        url: rootDir + "../include/header.html",
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function (html) {
+            html = html.replace(/\{\$root\}/g, rootDir);
+            $(".headerContainer").html(html);
+        }
+    });
+}
+
+function footer(rootDir) {
+    $.ajax({
+        url: rootDir + "../include/footer.html",
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function (html) {
+            html = html.replace(/\{\$root\}/g, rootDir);
+            $(".footerContainer").html(html);
+        }
+    });
+}
+
+function sidebar(rootDir) {
+    $.ajax({
+        url: rootDir + "../include/sidebar.html",
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function (html) {
+            html = html.replace(/\{\$root\}/g, rootDir);
+            $(".sidebarContainer").html(html);
+        }
+    });
+}
