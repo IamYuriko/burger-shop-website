8516fae41152814903fdae40dd5ff15c71dd16ba
IamYuriko <nrtyrk@gmail.com>
2024-05-11 00:39:13 +0900

    レスポンシブ対応済み

--- js/script.js
+++ js/script.js
@@ -49,3 +49,22 @@ function headerArchive(rootDir) {
         }
     });
 }
+
+//ハンバーガーメニューボタン開閉
+$(function () {
+    $(".l-header__openBtn").click(function () { //ボタンがクリックされたら
+        $(".l-sidebar").addClass("is-active"); //.l-sidebarに.is-activeを付与
+        $(".p-overlay-hamburgerMenu").addClass("is-active"); //.l-wrapper__overlayに.is-activeを付与
+        $("body").css("overflow", "hidden"); //bodyにoverflow: hidden;を指定 //position:fixed;でもOK
+    });
+});
+
+$(function () {
+    $(".p-sideMenu__closeBtn").click(function () { //ボタンがクリックされたら
+        $(".l-sidebar").removeClass("is-active"); //.l-sidebarの.is-activeを削除
+        $(".p-overlay-hamburgerMenu").removeClass("is-active"); //.l-wrapper__overlayの.is-activeを削除
+        $("body").css("overflow", ""); //bodyからoverflow: hidden;を解除
+    });
+});
+
+
